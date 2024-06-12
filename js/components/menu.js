37f23f151a3c227d7dd0fa090b28d314a471a46d
ealexandergarcia <106351807+ealexandergarcia@users.noreply.github.com>
2024-06-12 09:00:28 -0500

    feat: :sparkles: Creacion de inserción de html mediante js para las categorias

---
+++ js/components/menu.js
@@ -0,0 +1,14 @@
+export const menuListCategoryIndex = async (res)=>{
+    let {data} = res;
+    let plantilla = "";
+    data.forEach(element => {
+        plantilla+=/*html*/` 
+        <li title="${element.name}">
+            <a href="#">
+                <img src="storage/img/category.svg" alt="">
+                <span>${element.name}</span>
+            </a>
+        </li>`
+    });
+    return plantilla
+}
--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
-import { getAllProductsName } from "./module/app.js";
+import { menuListCategoryIndex } from "./components/menu.js";
 import { galleryIndex } from "./components/gallery.js";
+import { getAllProductsName, getAllCategory } from "./module/app.js";
 
 let input_search = document.querySelector("#input_search");
 let main__article = document.querySelector(".main__article")
+let nav__ul = document.querySelector(".nav__ul")
+
+addEventListener("DOMContentLoaded", async (e) =>{
+    let data = await getAllCategory();
+    nav__ul.innerHTML = await menuListCategoryIndex(data);
+})
 
 input_search.addEventListener("change", async e => {
     let data = { search: e.target.value }
