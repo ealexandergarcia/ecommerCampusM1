aa6a12b076ecb0910969206d19e4b9e59c74f31d
ealexandergarcia <106351807+ealexandergarcia@users.noreply.github.com>
2024-06-12 08:33:11 -0500

    refactor: :hammer: cambio en la iteracion para la generacion de contenido html mediante js

--- js/components/gallery.js
+++ js/components/gallery.js
@@ -1,20 +1,27 @@
-export const galleryIndex = (arg) => {
-    return /*html*/` 
-    <section>
-        <div class="section__front_page">
-            <a href="views/detail.html">
-                <img src=${arg.product_photo} alt="">
-            </a>
-            <img src="storage/img/heart.svg" alt="">
-        </div>
-        <h5>${arg.product_title}</h5>
-        <small>Dress modern</small>
-        <div class="section__price">
-            <span>${arg.product_price}</span>
-            <div class="price__score">
-                <img src="storage/img/star.svg" alt="">
-                <p>${arg.product_star_rating}</p>
+export const galleryIndex = (res) => {
+    let {products} = res.data;
+    let plantilla = "";
+    
+    products.forEach(element => {
+        plantilla += /*html*/` 
+        <section>
+            <div class="section__front_page">
+                <a href="views/detail.html">
+                    <img src=${element.product_photo} alt="">
+                </a>
+                <img src="storage/img/heart.svg" alt="">
             </div>
-        </div>
-    </section>`
+            <h5>${element.product_title}</h5>
+            <small>Dress modern</small>
+            <div class="section__price">
+                <span>${element.product_price}</span>
+                <div class="price__score">
+                    <img src="storage/img/star.svg" alt="">
+                    <p>${element.product_star_rating}</p>
+                </div>
+            </div>
+        </section>`
+    });
+
+    return plantilla
 }
--- js/main.js
+++ js/main.js
@@ -5,12 +5,10 @@ let input_search = document.querySelector("#input_search");
 let main__article = document.querySelector(".main__article")
 
 input_search.addEventListener("change", async e => {
-    let data = { search : e.target.value}
+    let data = { search: e.target.value }
     input_search.value = null;
     let res = await getAllProductsName(data);
-    let products = res.data.products
-    products.forEach(element => {
-        main__article.innerHTML += galleryIndex(element);
-    });
-    console.log(products);
+
+    main__article.innerHTML += galleryIndex(res);
+
 });
