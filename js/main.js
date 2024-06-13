78c9e93119ede8ff26ce12a45fcac904363738bb
ealexandergarcia <106351807+ealexandergarcia@users.noreply.github.com>
2024-06-13 07:39:50 -0500

    refactor: :hammer: cambio en la logica de consumo en la api
    Se implemento una forma para que la api solo se consuma una vez y quede guardada en el localstorage

--- js/main.js
+++ js/main.js
@@ -1,21 +1,24 @@
 import { menuListCategoryIndex } from "./components/menu.js";
 import { galleryIndex } from "./components/gallery.js";
-import { getAllProductsName, getAllCategory } from "./module/app.js";
+import { getAllProductsName, getAllCategory, getAllProductsByCategory } from "./module/app.js";
 
 let input_search = document.querySelector("#input_search");
 let main__article = document.querySelector(".main__article")
 let nav__ul = document.querySelector(".nav__ul")
 
 addEventListener("DOMContentLoaded", async (e) =>{
-    let data = await getAllCategory();
-    nav__ul.innerHTML = await menuListCategoryIndex(data);
+    if (!localStorage.getItem("getAllCategory")) localStorage.setItem("getAllCategory", JSON.stringify(await getAllCategory())); 
+    nav__ul.innerHTML = await menuListCategoryIndex(JSON.parse(localStorage.getItem("getAllCategory")));
 })
 
 input_search.addEventListener("change", async e => {
-    let data = { search: e.target.value }
+    let params = new URLSearchParams(location.search);
+    let data = { search: e.target.value, id:params.get("id") }
+    
     input_search.value = null;
     let res = await getAllProductsName(data);
-
-    main__article.innerHTML += galleryIndex(res);
+    // console.log(res);
+    
+    main__article.innerHTML += galleryIndex(res,params.get("id"));
 
 });
--- js/module/app.js
+++ js/module/app.js
@@ -5,18 +5,31 @@ export const getAllProductsName = async ({ search: text } = { search: "Phone" })
         method: 'GET',
         headers
     };
-    let res = await fetch(url,options);
+    let res = await fetch(url, options);
     let data = await res.json();
     return data;
 }
 
-export const getAllCategory= async ()=>{
+export const getAllCategory = async () => {
     const url = 'https://real-time-amazon-data.p.rapidapi.com/product-category-list?country=US';
     const options = {
         method: 'GET',
         headers
     };
-    let res = await fetch(url,options);
+    let res = await fetch(url, options);
     let data = await res.json();
     return data;
 }
+
+
+export const getAllProductsByCategory = async ({ search: text, id:idCategory }) => {
+    const url =  `https://real-time-amazon-data.p.rapidapi.com/search?query=${text}&page=1&country=US&sort_by=RELEVANCE&category_id=${idCategory}&product_condition=ALL ` ;
+    const options = {
+        method: 'GET',
+        headers
+    };
+    let res = await fetch(url, options);
+    let data = await res.json();
+    return data;
+}
+
