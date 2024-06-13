7b228e8b285b467ff66d80b32735f50c838d01d1
ealexandergarcia <106351807+ealexandergarcia@users.noreply.github.com>
2024-06-13 07:38:35 -0500

    feat: :sparkles: Asignacion de id al boton

--- js/components/menu.js
+++ js/components/menu.js
@@ -4,7 +4,7 @@ export const menuListCategoryIndex = async (res)=>{
     data.forEach(element => {
         plantilla+=/*html*/` 
         <li title="${element.name}">
-            <a href="#">
+            <a href="?id=${element.id} ">
                 <img src="storage/img/category.svg" alt="">
                 <span>${element.name}</span>
             </a>
