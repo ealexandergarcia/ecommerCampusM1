76b80cf2de2fe3d9a5ad5d7d64a7d5f8280a2b1a
ealexandergarcia <106351807+ealexandergarcia@users.noreply.github.com>
2024-06-13 08:54:58 -0500

    feat: :construction: Creacion de una indexdb, creacion de paquetes para la parte de detalles del producto

--- js/detail.js
+++ js/detail.js
@@ -1,6 +1,5 @@
 import { getProductId } from "./module/app.js"
 
-
 addEventListener("DOMContentLoaded", async (e)=>{
     let params = new URLSearchParams(location.search);
     console.log(params.get("id"));
