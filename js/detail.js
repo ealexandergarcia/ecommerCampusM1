bff9313bc514edf15b7bb7535fda4f7ca1c70104
ealexandergarcia <106351807+ealexandergarcia@users.noreply.github.com>
2024-06-16 17:31:07 -0500

    feat: :sparkles: Guardar producto seleccionado en el sesion storage

--- js/detail.js
+++ js/detail.js
@@ -1,18 +1,24 @@
 import { galleryCategory } from "./components/gallery.js";
-import { titleProductDetail } from "./components/section.js";
+import { buttonAtcProductDetial, titleProductDetail } from "./components/section.js";
 import { getProductId } from "./module/detail.js";
 
 
-let main__section_gallery = document.querySelector("#main__section_gallery");
-let main__section_title = document.querySelector("#main__section_title");
-
+let main__section__gallery = document.querySelector("#main__section__gallery");
+let main__section__title = document.querySelector("#main__section__title");
+let footer__ul = document.querySelector(".footer__ul");
+let info = "";
+let prueba = {};
 addEventListener("DOMContentLoaded", async(e)=>{
     let params = new URLSearchParams(location.search);
     let id = params.get('id');
     if(!localStorage.getItem(id)) localStorage.setItem(id, JSON.stringify(await getProductId({id})));
-    let info = JSON.parse(localStorage.getItem(id));
+    info = JSON.parse(localStorage.getItem(id));
+    console.log(info);
+    console.log(localStorage)
+    prueba.Producto = info;
     main__section__gallery.innerHTML = await galleryCategory(info)
     main__section__title.innerHTML = await titleProductDetail(info)
+    footer__ul.innerHTML = await buttonAtcProductDetial(info)
     // let {data} = res;
     // let {
     //     category_path,
@@ -26,4 +32,12 @@ addEventListener("DOMContentLoaded", async(e)=>{
     //     ...dataUpdate
     // } = data;
     // console.log(dataUpdate);
+})
+
+footer__ul.addEventListener("click", async (e) =>{
+    let params = new URLSearchParams(location.search);
+    let id = params.get('id');
+    console.log(id)
+    if(!sessionStorage.getItem(prueba.Producto)) sessionStorage.setItem(id,JSON.stringify(prueba.Producto));
+    let info = JSON.parse(sessionStorage.getItem(id));
 })
