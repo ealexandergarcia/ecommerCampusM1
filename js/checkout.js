b0726da881b1e5341d640024e12acd4bfa582564
ealexandergarcia <106351807+ealexandergarcia@users.noreply.github.com>
2024-06-19 08:00:19 -0500

    feat: :sparkles: Generacion de contenido de checkout mediente js

--- js/checkout.js
+++ js/checkout.js
@@ -1,71 +1,88 @@
-// import { cardProductCheckout, billProductCheckout } from "./components/section.js";
+import { cardProductCheckout, billProductCheckout } from "./components/section.js";
 
-// let sessionStorageValues = Object.values(sessionStorage);
-// let checkout__details = document.querySelector(".checkout__details");
-// let bill__section = document.querySelector(".section__bill"); // Asume que tienes un contenedor para la sección de la factura
+let sessionStorageValues = Object.values(sessionStorage);
+console.log(sessionStorageValues);
 
-// addEventListener("DOMContentLoaded", async (e) => {
-//     checkout__details.innerHTML = await cardProductCheckout(sessionStorageValues);
+let checkout__details = document.querySelector(".checkout__details");
+let bill__section = document.querySelector(".section__bill"); // Asume que tienes un contenedor para la sección de la factura
 
-//     // Seleccionar todos los elementos de incremento, decremento y valores de contador
-//     let incrementButtons = document.querySelectorAll(".increment");
-//     let decrementButtons = document.querySelectorAll(".decrement");
-//     let counterValues = document.querySelectorAll(".counter__value");
-//     let totalPriceElements = document.querySelectorAll(".total-price");
-//     let unitPriceElements = document.querySelectorAll(".unit-price");
+addEventListener("DOMContentLoaded", async (e) => {
+    checkout__details.innerHTML = await cardProductCheckout(sessionStorageValues);
 
-//     // Inicializar la factura
-//     updateBillSection();
+    // Seleccionar todos los elementos de incremento, decremento y valores de contador
+    let incrementButtons = document.querySelectorAll(".increment");
+    let decrementButtons = document.querySelectorAll(".decrement");
+    let counterValues = document.querySelectorAll(".counter__value");
+    let totalPriceElements = document.querySelectorAll(".total-price");
+    let unitPriceElements = document.querySelectorAll(".unit-price");
 
-//     // Añadir event listeners a cada par de botones de incremento y decremento
-//     incrementButtons.forEach((incrementButton, index) => {
-//         let counterValue = counterValues[index];
-//         let totalPriceElement = totalPriceElements[index];
-//         let unitPrice = parseFloat(unitPriceElements[index].getAttribute('data-price'));
+    // Inicializar la factura
+    updateBillSection();
 
-//         incrementButton.addEventListener("click", (e) => {
-//             e.preventDefault(); // Prevenir el comportamiento por defecto del enlace
-//             let currentValue = parseInt(counterValue.textContent);
-//             counterValue.textContent = currentValue + 1;
-//             totalPriceElement.textContent = `$${(unitPrice * (currentValue + 1)).toFixed(2)}`;
-//             updateBillSection();
-//         });
-//     });
+    // Añadir event listeners a cada par de botones de incremento y decremento
+    incrementButtons.forEach((incrementButton, index) => {
+        let counterValue = counterValues[index];
+        let totalPriceElement = totalPriceElements[index];
+        let unitPrice = parseFloat(unitPriceElements);
 
-//     decrementButtons.forEach((decrementButton, index) => {
-//         let counterValue = counterValues[index];
-//         let totalPriceElement = totalPriceElements[index];
-//         let unitPrice = parseFloat(unitPriceElements[index].getAttribute('data-price'));
-
-//         decrementButton.addEventListener("click", (e) => {
-//             e.preventDefault(); // Prevenir el comportamiento por defecto del enlace
-//             let currentValue = parseInt(counterValue.textContent);
-//             if (currentValue > 1) {
-//                 counterValue.textContent = currentValue - 1;
-//                 totalPriceElement.textContent = `$${(unitPrice * (currentValue - 1)).toFixed(2)}`;
-//                 updateBillSection();
-//             }
-//         });
-//     });
-// });
+        incrementButton.addEventListener("click", (e) => {
+            let currentValue = parseInt(counterValue.textContent);
+            e.preventDefault(); // Prevenir el comportamiento por defecto del enlace
+            console.log(currentValue)
+            counterValue += 1;
+            totalPriceElement.textContent = `${(unitPrice * (currentValue + 1)).toFixed(2)}`;
+            updateBillSection();
+            sessionStorageValues.forEach(element => {
+                if (element !== null && typeof element === 'string') {
+                    const data = JSON.parse(element);
+                    let info = data.data;
+                    if (data.status === 'OK' && data.request_id && info){
+                        console.log(info);
+                        info.quantity = currentValue;
+                        sessionStorage.setItem(info.asin,JSON.stringify(info));
+                    }
+                }
+            });
 
 
-// // Función para actualizar la sección de la factura
-// const updateBillSection = async () => {
-//     let totalItems = 0;
-//     let totalPrice = 0;
+        });
+    });
 
-//     let counterValues = document.querySelectorAll(".counter__value");
-//     let unitPriceElements = document.querySelectorAll(".unit-price");
+    decrementButtons.forEach((decrementButton, index) => {
+        let counterValue = counterValues[index];
+        let totalPriceElement = totalPriceElements[index];
+        let unitPrice = parseFloat(unitPriceElements[index].getAttribute('data-price'));
 
-//     counterValues.forEach((counterValue, index) => {
-//         let count = parseInt(counterValue.textContent);
-//         let unitPrice = parseFloat(unitPriceElements[index].getAttribute('data-price'));
+        decrementButton.addEventListener("click", (e) => {
+            e.preventDefault(); // Prevenir el comportamiento por defecto del enlace
+            let currentValue = parseInt(counterValue.textContent);
+            if (currentValue > 1) {
+                counterValue.textContent = currentValue - 1;
+                totalPriceElement.textContent = `$${(unitPrice * (currentValue - 1)).toFixed(2)}`;
+                updateBillSection();
+                
+            }
+        });
+    });
+});
 
-//         totalItems += count;
-//         totalPrice += count * unitPrice;
-//     });
 
-//     bill__section.innerHTML = await billProductCheckout(totalItems, totalPrice);
-// };
+// Función para actualizar la sección de la factura
+const updateBillSection = async () => {
+    let totalItems = 0;
+    let totalPrice = 0;
+
+    let counterValues = document.querySelectorAll(".counter__value");
+    let unitPriceElements = document.querySelectorAll(".unit-price");
+
+    counterValues.forEach((counterValue, index) => {
+        let count = parseInt(counterValue.textContent);
+        let unitPrice = parseFloat(unitPriceElements[index].getAttribute('data-price'));
+
+        totalItems += count;
+        totalPrice += count * unitPrice;
+    });
+
+    bill__section.innerHTML = await billProductCheckout(totalItems, totalPrice);
+};
 
