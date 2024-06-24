2a5f11d277941570eb1e79d8287be2af14770d87
ealexandergarcia <106351807+ealexandergarcia@users.noreply.github.com>
2024-06-24 06:49:42 -0500

    chore: :hammer: cambio de apikey

--- js/components/env.js
+++ js/components/env.js
@@ -1,4 +1,4 @@
 export const headers = {
-    'x-rapidapi-key': '949e10eb89msh556598de108e35dp1398bdjsn0364cd072166',
+    'x-rapidapi-key': '14a1f8eb64mshdcc29dfaaed8244p14ec59jsn9ffc06799e27',
     'x-rapidapi-host': 'real-time-amazon-data.p.rapidapi.com'
 }
